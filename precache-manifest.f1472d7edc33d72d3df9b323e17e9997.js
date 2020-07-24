a6d2c4da650db0819cba99838e058987c4fc0de9
Marcus Vinícius Ribeiro do Nascimento <peo_manascimento@uolinc.com>
2020-07-24 19:12:39 -0300

    Updates

---
+++ precache-manifest.f1472d7edc33d72d3df9b323e17e9997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b99631e67bf10a8c0bf0e03aea39b58",
+    "url": "/jogo-da-velha/index.html"
+  },
+  {
+    "revision": "52e2f3b08e787059eaf7",
+    "url": "/jogo-da-velha/static/css/main.68a13101.chunk.css"
+  },
+  {
+    "revision": "7bcc73532a58b8d1baf5",
+    "url": "/jogo-da-velha/static/js/2.a6030540.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jogo-da-velha/static/js/2.a6030540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e2f3b08e787059eaf7",
+    "url": "/jogo-da-velha/static/js/main.1ea689bf.chunk.js"
+  },
+  {
+    "revision": "1cc895a0e126859dd46b",
+    "url": "/jogo-da-velha/static/js/runtime-main.9562fd20.js"
+  }
+]);
